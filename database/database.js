5ee8c85c6d921b1847d7148d850eda919568f9fa
Silas Sena <silas.guimaraes@smartnx.io>
2022-01-14 17:04:59 -0300

    removendo tudo

--- database/database.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize');
-
-//configurando a conexao
-const connection = new Sequelize('guiaperguntas','root','123456',{
-    hots:'localhost',
-    dialect:'mysql'
-});
-
-module.exports = connection;
